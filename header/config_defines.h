48b303ad272f33c1eac60c85a5d16930da056cf8
BeFra <benjamin.frank@htwg-konstanz.de>
2015-02-04 00:33:09 +0100

    add header

---
+++ header/config_defines.h
@@ -0,0 +1,21 @@
+/*
+ * config_defines.h
+ *
+ * Created: 03.11.2014 13:59:28
+ *  Author: befrank
+ */ 
+
+
+#ifndef CONFIG_DEFINES_H_
+#define CONFIG_DEFINES_H_
+
+#define DEBUG
+
+#define SCL_CLOCK 400000L
+
+#ifndef F_CPU
+#define F_CPU 16000000UL //CPU Clock 16Mhz
+#endif
+
+
+#endif /* CONFIG_DEFINES_H_ */
